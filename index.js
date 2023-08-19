712fc80a796cda684fb5eb36387c8ca6c94ffa67
Tor Eirik Trandal <teitrand@hotmail.com>
2023-08-19 02:13:04 +0200

    Added reset button, maxScore and errormsg

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-
+let home = document.getElementById("home-score")
+let guest = document.getElementById("guest-score")
+let error = document.getElementById('error')
+let maxScore = 99
 
 function addPoint(homeTeam, score){
-    let home = document.getElementById("home-score")
-    let guest = document.getElementById("guest-score");
     if(homeTeam == 1){
-        home.innerText = parseInt(home.innerText) + score
+        if((parseInt(home.innerText) + score) <= maxScore){
+            home.innerText = parseInt(home.innerText) + score
+        }else{
+            home.innerText = maxScore
+            error.innerText = "Home team reached the max score"
+        }
     }else{
-        guest.innerText = parseInt(guest.innerText) + score
+        if((parseInt(guest.innerText) + score) <= maxScore){
+            guest.innerText = parseInt(guest.innerText) + score
+        }else{  
+            guest.innerText = maxScore
+            error.innerText = "Guest team reached the max score"
+        }
     }
+}
+
+function reset(){
+    home.innerText = 0
+    guest.innerText = 0;
 }
