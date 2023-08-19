bfcb35298e4465777d438735fb3208b194d1d1f9
Tor Eirik Trandal <teitrand@hotmail.com>
2023-08-19 12:38:37 +0200

    DRY'er code thanks to Naitik. Added reset error.

--- index.js
+++ index.js
@@ -3,25 +3,25 @@ let guest = document.getElementById("guest-score")
 let error = document.getElementById('error')
 let maxScore = 99
 
-function addPoint(homeTeam, score){
-    if(homeTeam == 1){
-        if((parseInt(home.innerText) + score) <= maxScore){
-            home.innerText = parseInt(home.innerText) + score
-        }else{
-            home.innerText = maxScore
-            error.innerText = "Home team reached the max score"
-        }
-    }else{
-        if((parseInt(guest.innerText) + score) <= maxScore){
-            guest.innerText = parseInt(guest.innerText) + score
-        }else{  
-            guest.innerText = maxScore
-            error.innerText = "Guest team reached the max score"
-        }
+function updateScore(element, score, teamName) {
+    if ((parseInt(element.innerText) + score) <= maxScore) {
+        element.innerText = parseInt(element.innerText) + score;
+    } else {
+        element.innerText = maxScore;
+        error.innerText = teamName + " team reached the max score.";
+    }
+}
+
+function addPoint(homeTeam, score) {
+    if (homeTeam == true) {
+        updateScore(home, score, "Home");
+    } else {
+        updateScore(guest, score, "Guest");
     }
 }
 
 function reset(){
     home.innerText = 0
-    guest.innerText = 0;
+    guest.innerText = 0
+    error.innerText = null
 }
